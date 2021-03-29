a335e5e12eea6f9cea19ac57f99ac4740c5c2aa8
Beatriz-Cruz1 <80706849+Beatriz-Cruz1@users.noreply.github.com>
2021-03-29 10:11:46 +0100

    Pong Update 1

--- Assets/Scripts/Scorekeeper.cs
+++ Assets/Scripts/Scorekeeper.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Scorekeeper : MonoBehaviour
 {
     int leftscore = 0;
     int rightscore = 0;
 
-    public void Goal()
+    [SerializeField]
+    TextMeshProUGUI leftscoreTxT;
+
+    [SerializeField]
+    TextMeshProUGUI rightscoreTXT;
+
+    public void Goal(Wall.Wallname parede)
     {
-        leftscore = leftscore + 1;
-        rightscore = rightscore + 1;
+        if (parede == Wall.Wallname.Right)
+        {
+            leftscore = leftscore + 1;
+            leftscoreTxT.text = leftscore.ToString();
+        } 
+        else if (parede == Wall.Wallname.Left)
+        {
+            rightscore = rightscore + 1;
+            rightscoreTXT.text = rightscore.ToString();
+        }
         Debug.Log(leftscore + " " + rightscore);
     }
     
--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class Wall : MonoBehaviour
 {
+    public enum Wallname { Left, Right }
+
     [SerializeField]
-    Scorekeeper scorekeeper;
+    Scorekeeper marcador;
+
+    [SerializeField]
+    Wallname wallname = Wallname.Left;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       /*
-        * Quando a bola bater, avisamos o Scorekeeper
-        * para aumentar a pontuação
-        */
+        /*
+         * Quando a bola bater, avisamos o Scorekeeper
+         * para aumentar a pontuação
+         */
+
+        marcador.Goal(wallname);
     }
+
 
 
 }
